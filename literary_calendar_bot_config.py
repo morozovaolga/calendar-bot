0f3d2ac32ef144c898bc187d954b1e4298466854
Olga Morozova <morozova31@gmail.com>
2025-12-07 12:32:46 +0300

    бновление конфигурации бота: изменение ID группы и времени отправки дайджеста

--- literary_calendar_bot_config.py
+++ literary_calendar_bot_config.py
@@ -10,13 +10,10 @@ BOT_TOKEN = "8470133642:AAHj5TBjRMDEtjGSCvwGpJot2vEUC_YEV-c"
 GRAPHQL_ENDPOINT = "https://svetapp-test.rusneb.ru/graphql"
 
 # ID группы в Telegram (можно получить через @userinfobot)
-GROUP_CHAT_ID = "5016711578"
-
-# URL календаря Yandex Calendar (если используется)
-CALENDAR_URL = "https://calendar.yandex.ru/export/html.xml?private_token=1c7f766fab8185a98f934a458b51e7fe8ff5b636&tz_id=Europe/Moscow&limit=90"
+GROUP_CHAT_ID = "-3330214920"
 
 # Время отправки ежедневного дайджеста (в часах, по умолчанию 12:00 дня)
-SEND_HOUR = 12
+SEND_HOUR = 13
 
 # Часовой пояс
 TIMEZONE = "Europe/Moscow"
